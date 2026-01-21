3be0af89ac86329bb4e45c0ab83865cf9e1cffa0
Miikari <miika.rauta@gmail.com>
2026-01-21 11:56:49 +0200

    1: Asteroids project start

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    i = 0
+    while i == 0:
+        log_state()
+
+        for event in pygame.event.get():
+            pass
+
+        screen.fill("black")
+        pygame.display.flip()
+    
+
+
+if __name__ == "__main__":
+    main()
